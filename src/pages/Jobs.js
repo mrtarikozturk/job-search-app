98c22987b64517638970d92fff279115b6bfc89e
mrtarikozturk <mr.tarik.ozturk@gmail.com>
2020-11-05 16:50:24 +0100

    Add JobItem component to Jobs page

--- src/pages/Jobs.js
+++ src/pages/Jobs.js
@@ -1,10 +1,50 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, { useState, useEffect } from 'react';
+import { View, Text, FlatList } from 'react-native';
+import axios from 'axios';
 
-const Jobs = () => {
+//My Import
+import { JobItem } from '../components';
+import { jobs } from '../style';
+
+const Jobs = (props) => {
+
+    const selectedLanguage = props.route.params.selectedLanguage;
+    // const {selectedLanguage} = props.route.params; // alternative way
+
+    //States
+    const [data, setData] = useState({});
+
+    const fetchData = async () => {
+        const response = await axios.get(`https://jobs.github.com/positions.json?search=${selectedLanguage.toLowerCase()}`);
+
+        setData(response.data);
+    }
+    console.log(data);
+
+    useEffect(() => { fetchData() }, []);
+
+    const renderJobs = ({ item }) => {
+
+        return (
+            <JobItem
+                data={item}
+                onPress={() => selectJob(item)}
+            />
+        )
+    }
+
+    const selectJob = (job) => { }
+
     return (
-        <View>
-            <Text>Jobs</Text>
+        <View style={jobs.container}>
+            <View style={jobs.header}>
+                <Text style={jobs.headerText}>JOBS FOR {selectedLanguage.toUpperCase()}</Text>
+            </View>
+            <FlatList
+                data={data}
+                renderItem={renderJobs}
+                key={(_, index) => index.toSting()}
+            />
         </View>
     )
 }
