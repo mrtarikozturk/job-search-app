d2adbf525aeff99681949f2d15f42286ae9690d6
mrtarikozturk <mr.tarik.ozturk@gmail.com>
2020-11-05 16:50:49 +0100

    Add style to JobItem component and Jobs page

--- src/style.js
+++ src/style.js
@@ -31,3 +31,40 @@ export const topicItem = StyleSheet.create({
         color: 'white',
     }
 })
+
+// Jobs Page
+export const jobs = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    header: {
+        height: Dimensions.get('window').height / 10,
+        backgroundColor: 'teal',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    headerText: {
+        textAlign: 'center',
+        fontSize: 20,
+        fontWeight: 'bold',
+        padding: 5,
+        color: 'white',
+    }
+
+});
+
+// JobItem Component
+export const jobItem = StyleSheet.create({
+    container: {
+        padding: 12,
+        margin: 7,
+        borderRadius: 6,
+        borderWidth: 1,
+        borderColor: '#bdbdbd',
+    },
+    title: {
+        fontWeight: 'bold',
+    },
+});
+
+
