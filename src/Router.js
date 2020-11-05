8b4eee7d0a8ca188a38b671c19475e265054dcc9
mrtarikozturk <mr.tarik.ozturk@gmail.com>
2020-11-05 09:11:31 +0100

    Add  @react-navigation/stack package

--- src/Router.js
+++ src/Router.js
@@ -1,10 +1,10 @@
-import React from 'react'
+import React from 'react';
 import { View, Text } from 'react-native'
 
 const Router = () => {
     return (
         <View>
-            <Text>Oppss</Text>
+            <Text>Opps</Text>
         </View>
     )
 }
